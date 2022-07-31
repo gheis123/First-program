b6c19ea4723dc7d22b03a7ac684b753c7893b865
gheis123 <54830941+gheis123@users.noreply.github.com>
2022-07-31 18:29:08 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,21 @@
 # First-program
-from math import *
-print(randint(1,15))
+'''
+Terminal
+
+1. git init
+
+2. git add .   ->전부다 copy 한다는 뜻.
+or git add (file_name.py)
+
+3. git status
+=>방금 추가한 정보를 알려준다.
+
+4. git commit -m "first commit"
+=>history create!
+
+5. git remote add origin https://github.com/gheis123/First-program.git
+
+6. git remote -v
+
+7. git push origin master 
+'''
